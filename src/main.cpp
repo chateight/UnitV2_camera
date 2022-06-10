ea665fdf59398b5b67a81f3a64b13e4839b0e7a5
ryuichi <rusami.usual@icloud.com>
2022-06-10 13:13:27 +0900

    # correct typo

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ void setup() {
   Grove.begin(115200);
   M5.Lcd.print("--initialized--");      // display M5 Lcd message
   M5.Lcd.setCursor(20, 70);
-  M5.Lcd.print("Face recongition");
+  M5.Lcd.print("Face recognition");
 }
 
 
